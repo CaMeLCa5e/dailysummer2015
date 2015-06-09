0be9db0a31b80ab0328d2a1d841c2dbe483b1703
Jack Martin <JM273606@gmail.com>
2015-06-08 23:44:11 -0400

    updates

--- loops.py
+++ loops.py
@@ -4,3 +4,47 @@ while a < 10:
 	print a
 else:
 	pass
+
+x = 10 
+while x != 0: 
+	print x 
+	x = x -1
+	print "Counting down..."
+print "and now the loop has ended."
+
+y = 1
+if y == 1: 
+	print "y = 1"
+
+print "even numbers up to 20"
+n = 1
+while n <= 20: 
+	if n % 2 == 0:
+		print n 
+	n = n +1 
+print "done"
+
+
+m = 1
+if m > 5: 
+	print "wrong"
+else: 
+	print "yep"
+
+z = 4
+if z > 80: 
+	print "something is wrong"
+elif z < 7: 
+	print "yep"
+
+d = 10 
+while d > 0: 
+	print d
+	if d > 5: 
+		print "big number"
+	elif a % 2 != 0:
+		print "odd number"
+	else: 
+		print "less than 5 and even"
+	d = d -1
+print "loop over"

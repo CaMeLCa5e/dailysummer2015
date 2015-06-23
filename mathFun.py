86eac80907e7d4611c45785673706989e18e2a7d
Jack Martin <JM273606@gmail.com>
2015-06-23 18:08:46 -0400

    from the docs

---
+++ mathFun.py
@@ -0,0 +1,4 @@
+import math
+print 'The value of PI is aprox {}.'.format(math.pi)
+print 'The value of PI is aprox {!r}.'.format(math.pi)
+print 'The value of PI is aprox {!s}.'.format(math.pi)
---
+++ substitutionVar.py
@@ -0,0 +1,3 @@
+x = 'Fred'
+print 'The story of {0}, {1}, and {other}.'.format(
+	'Bill', 'Manfred', other=x)

bb18ca2f7fbe058ba05d6b4fb69a1d43655cc290
Jack Martin <JM273606@gmail.com>
2015-07-08 23:09:55 -0400

    playing with code.

---
+++ argparse_test.py
@@ -0,0 +1,28 @@
+import argparse 
+
+def main2():
+	parser = argparse.ArgumentParser(description='Process some ints')
+	parser.add_argument('integers', metavar='N', type=int, nargs='+',
+		help='an int for the accumulator')
+	parser.add_argument('--sum', dest='accumulate', action='store_const',
+		const=sum, default=max, 
+		help='sum the ints (default: find the max)')
+
+	args = parser.parse_args()
+	print args.accumulate(args.integers)
+
+def main():
+	parser = argparse.ArgumentParser(description='AVMonitor master.')
+
+	parser.add_argument('action', choices=['update', 'revert', 'dispatch', 'test'])
+	parser.add_argument('--vm', required=False)
+	parser.add_argument('-p', '--pool', default=2)
+	args = parser.parse_args()
+
+	print args
+	print args.action
+	print args.vm
+
+if __name__ == '__main__':
+	main()
+	

7a5c2a19a2a7d82fc653db8cc6a8d2e4744e8dd7
Jack Martin <JM273606@gmail.com>
2015-07-30 23:31:43 -0400

    tests

---
+++ tests.scala
@@ -0,0 +1,18 @@
+
+import scala.collection.mutable
+
+scala.collection.immutable.list
+scala.List 
+List
+
+Traversable(1,2,3)
+Iterable("x", "y", "z")
+Map("x" -> 24, "y" -> 25, "z" -> 26)
+Set(Color.red, Color.green, Color.blue)
+SortedSet("hello", "world")
+Buffer(x, y, z)
+IndexedSeq(1.0, 2.0)
+LinearSeq(a, b, c)
+
+List(1, 2, 3)
+HashMap("x" -> 24, "y" -> 25, "z" -> 26)

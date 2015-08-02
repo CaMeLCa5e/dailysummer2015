495c3d61791536467085bb14d256d854fc12f213
Jack Martin <JM273606@gmail.com>
2015-08-01 20:28:38 -0400

    updates

--- foreach.scala
+++ foreach.scala
@@ -7,3 +7,13 @@ scala> val xs = List(1, 2, 3, 4, 5)
 xs: List[Int] = List(1, 2, 3, 4, 5)
 scala> val git = xs grouped 3
 git: Iterator[List[Int]] = non-empty iterator
+scala> git.next()
+res3: List[Int] = List(1, 2, 3)
+scala> sit.next()
+res4: List[Int] = (4, 5)
+scala> sit.next()
+res5: List[Int] = (1, 2, 3)
+scala> sit.next()
+res5: List[Int] = (2, 3, 4)
+scala> sit.next()
+res5: List[Int] = (3, 4, 5)

b36dd2a5055e22e22240d435ce553fe6511f56d9
Jack Martin <JM273606@gmail.com>
2015-08-13 09:18:41 -0400

    updates

--- fibb.py
+++ fibb.py
@@ -40,8 +40,24 @@ def memoize(fn, arg):
 		memo[arg] = fn(arg)
 		return memo[arg]
 
-fibm = memoize(fib, 5)
+fibm = memoize(fib, 85)
 
 print fibm
 
 
+# fib decorators 
+class Memoize:
+	def __init__(self, fn):
+		self.fn = fn
+		self.memo = {}
+	def __call__(self, arg):
+		if arg not in self.memo: 
+			self.memo[arg] = self.fn(arg)
+			return self.memo[arg]
+@Memoize 
+def fib(n): 
+	a, b = 1, 1
+	for i in range(n-1): 
+		a,b = b, a+b 
+	return a 
+print fib(25)		

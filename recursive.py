3d291c5a5474647d448229df5203899393ba5330
Jack Martin <JM273606@gmail.com>
2015-08-15 19:33:25 -0400

    recursive updates

--- recursive.py
+++ recursive.py
@@ -8,3 +8,11 @@ def factorial(n):
         print("intermediate result")
         return res
 print (factorial(5))
+
+
+def iterative_factorial(n):
+    result = 1
+    for i in range(2, n+1):
+        result * = i
+    return result
+    

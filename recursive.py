6deeb323fab2eaee8c568ceb98688ef5977db21b
Jack Martin <JM273606@gmail.com>
2015-08-16 14:29:16 -0400

    updates

--- recursive.py
+++ recursive.py
@@ -15,4 +15,19 @@ def iterative_factorial(n):
     for i in range(2, n+1):
         result * = i
     return result
-    
+
+def fib(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fib(n-1)+fib(n-2)
+
+def fibi(n):
+    old, new = 0, 1
+    if n == 1:
+        return 0
+    for i in range (n-1):
+        old, new = new, old + new
+    return new
